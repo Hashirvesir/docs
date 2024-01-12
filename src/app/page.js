3f174601104ef0ea15cb2c7c23a56bee89f8b1f0
Hashirvesir <hashirvesir123@gmail.com>
2024-01-12 22:32:58 +0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+
+import Background from "@/components/Background"
+import Foreground from "@/components/Foreground"
+Foreground
+const Home = () => {
+  return (
+    <>
+     <Background/>
+      <Foreground/>
+    </>
+  )
+}
+
+export default Home
+
